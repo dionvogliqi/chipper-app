f5344725433931e06d40bf813f32d58493e0ce4a
Dion Vogliqi <dionvogliqi@gmail.com>
2026-01-08 22:57:30 +0100

    Poll posts from the API each 30 seconds

--- pages/index.vue
+++ pages/index.vue
@@ -7,11 +7,35 @@ const user = useUser()
 const posts = usePosts()
 
 await posts.fetch()
+
+let pollInterval = null
+
+onMounted(() => {
+  pollInterval = setInterval(() => {
+    posts.poll()
+  }, 30000)
+})
+
+onUnmounted(() => {
+  if (pollInterval) {
+    clearInterval(pollInterval)
+  }
+})
+
+function handleLoadNewPosts() {
+  posts.loadNewPosts()
+}
 </script>
 
 <template>
   <PostForm
     v-if="!user.isGuest" />
+  <button
+    v-if="posts.hasNewPosts"
+    class="w-full bg-blue-500 text-white font-bold py-3 px-4 rounded-lg mb-8 hover:bg-blue-600 transition-colors"
+    @click="handleLoadNewPosts">
+    Load New Posts
+  </button>
   <div class="grid gap-16">
     <PostItem
       v-for="post in posts.data"
--- stores/posts.js
+++ stores/posts.js
@@ -1,16 +1,48 @@
 export const usePosts = defineStore('posts', () => {
-  const { $api } = useNuxtApp()
-
   const data = ref([])
-
+  const pendingPosts = ref([])
   const loading = ref(false)
 
+  const hasNewPosts = computed(() => pendingPosts.value.length > 0)
+
   async function fetch () {
+    const { $api } = useNuxtApp()
     const response = await $api.get('/posts')
     data.value = response.data
   }
 
+  async function poll () {
+    const { $api } = useNuxtApp()
+
+    try {
+      const response = await $api.get('/posts')
+      const newPosts = response.data
+
+      if (data.value.length === 0) {
+        data.value = newPosts
+        return
+      }
+
+      const latestId = data.value[0]?.id
+      const freshPosts = newPosts.filter(post => post.id > latestId)
+
+      if (freshPosts.length > 0) {
+        pendingPosts.value = freshPosts
+      }
+    } catch (e) {
+      console.error('Failed to poll posts:', e)
+    }
+  }
+
+  function loadNewPosts () {
+    if (pendingPosts.value.length > 0) {
+      data.value = [...pendingPosts.value, ...data.value]
+      pendingPosts.value = []
+    }
+  }
+
   async function create ({ title, body }) {
+    const { $api } = useNuxtApp()
     loading.value = true
 
     try {
@@ -24,8 +56,12 @@ export const usePosts = defineStore('posts', () => {
 
   return {
     data,
+    pendingPosts,
     loading,
+    hasNewPosts,
     fetch,
+    poll,
+    loadNewPosts,
     create
   }
 })
